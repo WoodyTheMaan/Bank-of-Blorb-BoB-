eaf5dcdb1d5f91cae16ba514d1cd4b693b045aca
WoodyTheMaan <buzz72woody@gmail.com>
2023-10-23 10:31:58 -0400

    Made some more stuff

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Bank_of_Blorb__BoB_
+﻿using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+
+namespace Bank_of_Blorb__BoB_
 {
     internal class Program
     {
+        public static double money = 150.0;
         static void Main(string[] args)
         {
+
+            //Program works best in full screen, whithout fullscreen the letters don't appear fully
             Console.WriteLine("Thanks for choosing Bank of Blorb, if you do not yet have an account with us you must create an account before using this ATM");
             Console.WriteLine("Please press enter, by doing so the enter key will scan your finger print. (Please note, you must use the same finger you used when registering)");
             Thread.Sleep(500);
@@ -32,9 +38,264 @@
             Thread.Sleep(700);
             Console.WriteLine("Succsesfull! Welcome back, Zorblaxianthor!");
             Console.WriteLine("Please enter you password:");
-            Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("PLEASE WAIT WHILE WE COMFIRM THIS IS YOUR PASSWORD...");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.WriteLine("COMFIRMED Welcome back to BoB Zorblaxianthor!");
+            Console.WriteLine("___________________________________________________________");
+            Menu();
+
         }
-        
-       
+        public static void Menu()
+        {
+            string userInput;
+            Console.WriteLine($"You have {money} Blorbian dollars");
+            Thread.Sleep(500);
+            Console.WriteLine("Please choose an option");
+            Console.WriteLine("");
+            Console.WriteLine("1 ------- Deposit");
+            Console.WriteLine("2 ------- Withdrawal");
+            Thread.Sleep(300);
+            Console.WriteLine("3 ------- Bill Payment");
+            Console.WriteLine("4 ------- Account Balance Update");
+            Console.WriteLine("5 ------- Exit");
+            Thread.Sleep(400);
+            Console.WriteLine("========================================");
+            Console.WriteLine("PLEASE NOTE: INVALID RESPONSE GIVEN WILL RESULT IN FEES SUBTRACTING FROM YOUR ACCOUNT");
+            Console.WriteLine("proteted by terms and conditions. See website to read.");
+            userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                Console.WriteLine("You have chosen Deposit, please wait while we send you to the deposit server");
+                Thread.Sleep(500);
+                Console.WriteLine("Fetching server...");
+                Thread.Sleep(700);
+                Console.WriteLine("Loading attachments...");
+                Thread.Sleep(400);
+                Console.WriteLine("Connected!");
+                Thread.Sleep(300);
+                Deposit();
+            }
+            else if (userInput == "2")
+            {
+
+                Console.WriteLine("You have chosen Withdeawl, please wait while we send you to the Withdrawl server");
+                Thread.Sleep(500);
+                Console.WriteLine("Fetching server...");
+                Thread.Sleep(700);
+                Console.WriteLine("Loading attachments...");
+                Thread.Sleep(400);
+                Console.WriteLine("Connected!");
+                Thread.Sleep(300);
+                Withdrawl();
+            }
+            else if (userInput == "3")
+            {
+                Console.WriteLine("You have chosen Bill Payment, please wait while we send you to the Bill Payment server");
+                Thread.Sleep(500);
+                Console.WriteLine("Fetching server...");
+                Thread.Sleep(700);
+                Console.WriteLine("Loading attachments...");
+                Thread.Sleep(400);
+                Console.WriteLine("Connected!");
+                Thread.Sleep(300);
+                BillPayment();
+            }
+            else if (userInput == "4")
+            {
+                Console.WriteLine("You have chosen Account Balance Update, please wait while we send you to the Account Balance Update server");
+                Thread.Sleep(500);
+                Console.WriteLine("Fetching server...");
+                Thread.Sleep(700);
+                Console.WriteLine("Loading attachments...");
+                Thread.Sleep(400);
+                Console.WriteLine("Connected!");
+                Thread.Sleep(300);
+                AccountUpdate();
+            }
+            else if (userInput == "5")
+            {
+                Exit();
+            }
+            else
+            {
+                NotValid();
+            }
+        }
+
+        public static void Deposit()
+        {
+            Console.Clear();
+            Console.WriteLine("Please insert money and press enter. (Note, this transaction will cost a fee of 0.75 to do)");
+            Console.ReadLine();
+            Console.WriteLine("Reading money.....");
+            Thread.Sleep(850);
+            money = money + 82;
+            Console.WriteLine("82 Blorbian dollars succssfully added to your account, thanks!");
+            Console.WriteLine($"You now have {money}");
+            Console.WriteLine($"Click enter to exit");
+            Console.ReadLine();
+            Exit();
+
+        }
+
+        public static void Withdrawl()
+        {
+            double withdrawlAmount;
+            Console.Clear();
+            Console.WriteLine("Please enter the amount of blorbian dollars you would like to withdrawl (Note, this transaction will cost a fee of 0.75 to do)");
+            withdrawlAmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Waiting for request to be processed...");
+            Thread.Sleep(1000);
+            if (money == 0)
+            {
+                Console.WriteLine("You don't have the funds to use anything, please press enter to exit.");
+                Console.ReadLine();
+                Exit();
+            }
+            money = money - 0.75;
+
+            if (withdrawlAmount > money)
+            {
+                Console.WriteLine("Request Failed...");
+                Console.WriteLine("If you withdrawled less Bloarbian dollars than you have please come to a branch to get things sorted");
+                Thread.Sleep(700);
+                Console.WriteLine("Removing 0.75 from your account");
+                Thread.Sleep(500);
+                Console.WriteLine($"You now have {money}, please click enter to exit.");
+                Console.ReadLine();
+                Exit();
+            }
+            else
+            {
+                money = money - withdrawlAmount;
+                Console.WriteLine($"{withdrawlAmount} of Blorbian Dollars has been withdrawled, money should be below you, Thanks!");
+                Console.WriteLine($"You now have {money}");
+                Console.WriteLine($"Click enter to exit");
+                Console.ReadLine();
+
+
+            }
+
+            Exit();
+        }
+
+        public static void BillPayment()
+        {
+            string yOrN;
+            double billAmountPaid, blorbcoinDepository = 20.12;
+            Console.WriteLine("Welcome, to Bill Payment! You have ONE outstanding bill from BLORBCOIN DEPOSITORY");
+            Console.WriteLine("The bill is 20.12 do you wish to pay for it now? (Y/N)");
+            yOrN = Console.ReadLine().ToLower();
+            if (yOrN == "y")
+            {
+
+
+
+                Console.WriteLine("How much do you want to pay? (Note: what you input is FINAL. INVALID RESPONSE WILL RESULT IN EXITING ATM)");
+                billAmountPaid = Convert.ToDouble(Console.ReadLine());
+                if (billAmountPaid < blorbcoinDepository)
+                {
+                    Thread.Sleep(700);
+                    Console.WriteLine($"Thanks! for paying {billAmountPaid}");
+                    Console.WriteLine("Subtracting 0.75 from account");
+                    money = money - 0.75;
+                    Console.WriteLine($"You now have {money}");
+                    Console.WriteLine("Click enter to exit");
+                    blorbcoinDepository = billAmountPaid - blorbcoinDepository;
+                    money = billAmountPaid - money;
+                    Console.ReadLine();
+                    Exit();
+                }
+                else if (billAmountPaid > blorbcoinDepository) 
+                {
+                    Thread.Sleep(700);
+                    Console.WriteLine("INVALID RESPONSE");
+                    Console.WriteLine("Subtracting 0.75 from account");
+                    money = money - 0.75;
+                    Console.WriteLine($"You now have {money}");
+                    Console.WriteLine("Click enter to exit");
+                    Console.ReadLine();
+                    Exit();
+                }
+
+
+
+                Thread.Sleep(500);
+                Console.WriteLine($"You paid {billAmountPaid} of Blorbian dollars");
+                Console.WriteLine($"You still must pay {blorbcoinDepository} amount of money");
+                Console.WriteLine("Click enter to exit");
+                Console.ReadLine();
+                Exit();
+
+
+            }
+            else if (yOrN == "n")
+            {
+                Console.WriteLine("You've selected 'No' please click enter to exit");
+                money = money - 0.75;
+                Console.WriteLine($"You now have {money}");
+                Console.ReadLine();
+                Exit();
+            }
+
+        }
+
+        public static void AccountUpdate()
+        {
+            Random generator = new Random();
+            string userInput;
+            double savingsAmount;
+            generator.Next();
+            savingsAmount = generator.Next(3, 91);
+            Console.WriteLine("Welcome to Account Balance Update!");
+            Console.WriteLine("1 ------- Chequing");
+            Console.WriteLine("2 ------- Savings");
+            userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                Console.WriteLine($"You've chosen chequing");
+                Console.WriteLine($"You have {money} amount of Blorbian Dollars in your chequing account");
+                money = money - 0.75;
+                Console.WriteLine($"Fee of 0.75 you now have {money}");
+                Console.WriteLine("Please press enter to exit");
+                Console.ReadLine();
+                Exit();
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine($"You've chosen savings");
+                Console.WriteLine($"You have {savingsAmount} amount of Blorbian Dollars in your savings account");
+                savingsAmount = savingsAmount - 0.75;
+                Console.WriteLine($"Fee of 0.75 you now have {savingsAmount}");
+                Console.WriteLine("Please press enter to exit");
+                Console.ReadLine();
+                Exit();
+            }
+        }
+
+        public static void NotValid()
+        {
+            Console.Clear();
+            Console.WriteLine("NOT A VALID INPUT. SUBTRACTING SEVENTY FIVE CENTS FROM ACCOUNT");
+
+            Console.WriteLine("Click enter to return to menu");
+            money = money - 0.75;
+            Console.ReadLine();
+            Menu();
+            
+        }
+
+        public static void Exit()
+        {
+            Console.Clear();
+            Console.WriteLine("Thanks for using BoB goodbye.");
+            Thread.Sleep(1500);
+
+        }
+
     }
-} 
+}
